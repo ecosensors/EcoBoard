abaeda7f417a3bb41f2de2b85f12a4295a63073c
EcoSensors <65593795+ecosensors@users.noreply.github.com>
2020-06-22 19:02:15 +0200

    Add files

---
+++ Ecoboard.cpp
@@ -0,0 +1,16 @@
+/*
+* That file is underdevelopment. Do not use it for now
+*/
+#include "Arduino.h"
+#include "Ecoboard.h"
+
+Ecoboard::Ecoboard()
+{
+	
+}
+
+int Ecoboard::begin()
+{
+}
+
+
---
+++ Ecoboard.h
@@ -0,0 +1,22 @@
+/*
+* That file is underdevelopment. Do not use it for now
+*/
+
+#ifndef Ecoboard_h
+#define Ecoboard_h
+
+#include "Arduino.h"
+
+class Ecoboard{
+ 	protected:
+ 		
+
+ 	private:
+		
+
+	public:
+		Ecoboard();
+		int begin();
+
+ };
+ #endif
