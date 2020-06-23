2c5759a4939aa1e9d3a96d403da032510dc1881c
EcoSensors <65593795+ecosensors@users.noreply.github.com>
2020-06-23 09:33:34 +0200

    Update

--- examples/expander-1wire/expander-1wire.ino
+++ examples/expander-1wire/expander-1wire.ino
@@ -1,14 +1,16 @@
 /*
+ * EcoSENSOR
+ * 
  * That file is still a draft
  * 
- * I/O EXPANDER PORT
+ * EXTENDER PORT
  * ===========================================================================
- * This is an example to use PCF8574 (expander port) with the EcoBoard)
+ * This is an example to use PCF8574 (extender port) with the EcoBoard)
  * You can easly use that example for an other application, but the codeand explication below is written for EcoBoard.
  * 
  * EcoBoard allow you to use 6 additonal GPIO with the PCF8574 ([Datasheeet](https://www.ti.com/product/PCF8574). 
  * The outputs P1 to P6, will allow you to control the status of another device or an LED. 
- * However, the outputs can only provide 50mA. In some cases, this power may not be sufficient. 
+ * However, the outputs can only provide 25mA. In some cases, this power may not be sufficient. 
  * It's the reason why, we added three MOSFET-P (IRML2244) on the outputs P1, P2 and P3. 
  * Thus, the outputs P1, P2 and P3 of the J3 connector can now supply up to 500mA. 
  * The P3, P5 and P6 outputs depend on the PCF8574.
