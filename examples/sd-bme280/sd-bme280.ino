9c4417cb07bf72d309c5a903340318b09d8236a6
EcoSensors <info@eco-sensors.ch>
2024-04-09 23:12:45 +0200

    Update sd-bme280.ino

--- examples/sd-bme280/sd-bme280.ino
+++ examples/sd-bme280/sd-bme280.ino
@@ -1,145 +1,99 @@
 /*
- * EcoSensors - SD, RTC, BME280 (I2C)
- * 
- * WARNING: This script is presented for informational purposes only and no warranty is given as to its suitability for 
- * your environment
- * The script will to be updated and improved following your needs
- * 
- * In that example, we will see how to create briefly a log file into the MicroSD card.
- * We will use a I2C barometer sensor (BME280), to record some value in the log file
- * 
- * We need a RTC module to get the time and to save the value into folders a the following
- * /log/20/06/24/212222.txt (/log/year/month/day/time.txt)
- * 
- * We will need to use the library Ecosensors (Ecosensors.ccp / Ecosensors.h)
- * I will try to provide comments and description, but you will need some knowledge to improve it, 
- * for you application
- *
- * WARNING: Before doing that example, we consider you alraedy calibrated the RTC as show here:
- * https://github.com/ecosensors/Ecoboard/blob/master/examples/rtc-lowpower/rtc-lowpower.ino
- * If you have not, make sure your board print am acceptable time and date
- *
+ * EcoSensors - SD, BME280 (I2C)
+ * The script is distributed WITHOUT WARRANTY.
  */
 
-#include <Ecoboard.h>
-#include <Wire.h>           // Need for I2C Bus
-/*
- * RTC
- */
-#include <RTClib.h>         // RTC is needed to get the time to create and to name the log file
-RTC_DS3231 rtc;
-int16_t y,m,d,h,mn,s;       // Used for RTC time
-int32_t unix_time;
-char date_time[18];         // 20-6-10 11:11:11
-bool isSdEnable = true;     // Enable SD card
+/* Libraries */
+#include <Ecoboard.h>               // Include the EcoBoard library (lib)
+#include <Wire.h>                   // Need for I2C Bus
+#include <RTClib.h>                 // Include the RTC lib for the clock
+#include <Adafruit_BME280.h>        // Include the BME280 lib for the barometer
+// Note: You do not need to include the SD lib, because the lib is included from the EcoSensors lib
 
 /* Interval */
-
 int32_t lastMeasure = 0;
-#define INTERVAL 5000 // = 5sec
+#define INTERVAL 5000               // Define an interval of 5sec between each loop
 unsigned long scheduler;
 
-
-/*
- * BME280
- */
+/* BME280 */
  #define SEALEVELPRESSURE_HPA (1013.25)
-#include <Adafruit_BME280.h>  // Import the library
-Adafruit_BME280 bme;          // Create an instence for I2C bus
+Adafruit_BME280 bme;                // Create an instence for I2C bus
 
-/*
- * SD is defined in the Ecoboard library
- */
+/* RTC */
+RTC_DS3231 rtc;                     // Intialize the RTC Clock library
+int16_t y,m,d,h,mn,s;               // Used for RTC time (y:year, m:month, d:day, h:hour, m:minutes, s:second)
+int32_t unix_time;                  // Used to store the time in unix format
+char date_time[18];                 // Used to store the time as the following: 20-6-10 11:11:11
 
-Ecoboard Eco(isSdEnable);
+/* SD */
+//The SD library is included from the Ecoboard library
+bool isSdEnable = true;             // Enable SD card or not
+bool debug = true;                  // Display the event from the library
+bool isRTCEnable = true;            // Enable the clock or no
+Ecoboard Eco(isSdEnable, isRTCEnable, debug);    // The SD card is initiate in the cpp file. See Eco.begin bellow
+
 
 void setup(void)
 {
   Serial.begin(9600);
   Wire.begin();  
 
-  delay(8000);
+  delay(8000);                      // Give a delay to open the terminal
 
-  /*
-   * If isSdEnable is true, Eco.begin will call the private function _sd_begin()
-   * to initiate the SD card, and list the content of the card
-   */
-  if(Eco.begin()==0)
-  {  
-    Serial.println(F("[Error]"));          // Errors here is mostly due to the SD card. Is it inserted?
-    while(1);
-  }
-
-  Serial.println("     ECOBOARD     ");
-  Serial.println(" SD card - BME280 ");
-  Serial.println("------------------");
+  Serial.println(" RTC / BME280 / SD card - BME280 ");
+  Serial.println("---------------------------------");
   Serial.println("");
 
-    /*
-   * We need to get the time from DS3231, in orderto create a log file into the SD card, named with the curent date/time
-   * To simplify, we consider you already calibrate the RTC, following that eample: https://github.com/ecosensors/Ecoboard/blob/master/examples/rtc-lowpower/rtc-lowpower.ino#L82
-   * look at #RTC_CALIBRATE
-   */
-  if(!rtc.begin())                        // Start Real Time Clock (RTC)
+  Eco.begin();                     // Begin the EcoBoard library
+
+  /* 
+  * RTC 
+  */
+  if(!rtc.begin())                  // Start Real Time Clock (RTC)
   {
-    Serial.println(F("[ERROR] Could not start RTC"));
-    while(true);
+    Serial.println(F("OK RTC Clock"));
+
+    DateTime now = rtc.now();         // Get the time
+    y = now.year();                   // Get the year
+    y = y-2000;                       // Change 2024 to 24
+    m = now.month();                  // Get the mounth
+    d = now.day();                    // Get the day
+    h = now.hour();                   // Get the hour
+    mn = now.minute();                // Get the minutes
+    s = now.second();                 // Get the second
+    unix_time = now.unixtime();       // Get the unix time
+
+    sprintf(date_time,"%i-%i-%i %i:%i:%i",y,m,d,h,mn,s);  // Save the value into the char date_time
+
+    Serial.print(F("Now is: "));      // Print the date/time
+    Serial.println(date_time);
   }
-
-  DateTime now = rtc.now();             // Get time
-
-  y = now.year();
-  y = y-2000;
-  m = now.month();
-  d = now.day();
-  h = now.hour();
-  mn = now.minute();
-  s = now.second();
-
-  unix_time = now.unixtime();
-  sprintf(date_time,"%i-%i-%i %i:%i:%i",y,m,d,h,mn,s);
-
-  Serial.print(F("Now is: "));
-  Serial.println(date_time);
-
-  /*
-   * Back to SD card preparation, now we are going to create the log file
-   */
-   if(Eco.sd_init_logFile(y,m,d,h,mn,s) !=  1)
+  else
   {
-    while(1);
+    Serial.println(F("KO Could not initialize RTC"));
   }
+  
 
+  /* 
+  * SD Card
+  */
+  if(Eco.sd_begin())                                              // Initiate the SD card
+  {  
+    Serial.println(F("OK SD Card"));
+  }
+  else
+  {
+    Serial.println(F("KO SD could not be initiazed or is inactive"));          // Errors here is mostly due to the SD card. Is it inserted?
+    Serial.println(F(".. Did you insert a card?"));
+    isSdEnable = false;
+  }
+  
+  
   /*
-   * Now the log file has been ceated.
-   */
-   //Eco.sd_write("Add new line, ");    // Modify that line following your neeed
-   //Eco.sd_writeln("add a new word at the same line and move the cursor to the next line");
+  * Initiate BME280 sensor
+  */
 
-   Serial.println(F("DONE!"));
-
-   /*
-    * Later I will show how to add a text in another file than the log file, but you can
-    * easly do it, working around those function (see /libraries/Ecoboard/Ecoboard.ccp):
-    * int Ecoboard::sd_writeln(char const * text)
-    * {
-    *   return _sd_write(_logFile, text, true); // _logFile is the path from root and name of the file
-    * }
-    *
-    * int Ecoboard::sd_write(char const * text)
-    * {
-    *   return _sd_write(_logFile, text, false);
-    * }
-    * 
-    * in Ecoboard.cpp and Ecoboard.h files
-    */
-
-
-    /*
-    * Initiate BME280 sensor
-    */
-
-    Serial.println(F("Init BME280"));
+  Serial.println(F("Init BME280"));
     bool status;
     status = bme.begin(); // 0x77 (default addr)
     // You can change the I2C addresss as the following: 
@@ -176,9 +130,6 @@ void loop(){
   {
     scheduler = millis() + INTERVAL;
 
-  // Interval with RTC module
-  //if(rtcInterval(lastMeasure, INTERVAL, 0) == true){
-
     // Only needed in forced mode! In normal mode, you can remove the next line.
     bme.takeForcedMeasurement(); // has no effect in normal mode
     Serial.print(F("Temperature = "));
@@ -198,43 +149,6 @@ void loop(){
     Serial.print(bme.readHumidity());
     Serial.println(F(" %"));
 
-    /*
-     * In progress
-    */
-
-
-    /*
-     // Uncomment this, if you want to use interval with RTC
-      DateTime now = rtc.now();
-      lastMeasure = now.unixtime();
-    */
-  }
-  
-}
-
-
-bool rtcInterval(int32_t lastTx, int32_t tx_interval, int debug)
-{ 
-    int32_t nextTx = lastTx + tx_interval;
-    int32_t unix_t;  
-    
-    DateTime t = rtc.now();
-
-    //Serial.println(now.unixtime());
-    unix_t = t.unixtime();
-    
-    if(debug==1){
-      Serial.print(unix_t);
-      Serial.print(F(" - "));
-      Serial.println(nextTx);
-    }
-
-    if(unix_t > nextTx)
-    {
-      return true;
-    }
-    else
-    {
-      return false;
-    }
+   
+  } 
 }
